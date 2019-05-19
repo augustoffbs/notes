bedf01d01799339747713401765d81bcb980e77c
Gordobs <augustoffbs@gmail.com>
2019-05-19 14:33:19 -0300

    j3j3j3j3

--- js.js
+++ js.js
@@ -2,6 +2,17 @@ var notes = JSON.parse(localStorage.getItem("notes"))
 
 var noteList = document.getElementById("notes-list");
 
+function getId () {
+    // Math.random should be unique because of its seeding algorithm.
+    // Convert it to base 36 (numbers + letters), and grab the first 9 characters
+    // after the decimal.
+    return '_' + Math.random().toString(36).substr(2, 9);
+  };
+
+if (notes == null) {
+    notes = [];
+}
+
 if(notes.length <= 0){
     console.log("zero");
     var zeroElements = document.createElement("div");
@@ -15,14 +26,16 @@ for (var n=0; n < notes.length; n++) {
     var noteContent = notes[n].content;
     var newElement = document.createElement('div');
     newElement.className = "note-item col-sm-2";
-    newElement.innerHTML = "<h5>" + noteName + "</h5>" + "<p>" + noteContent + "</p>";
+    newElement.innerHTML = "<h5>" + noteName + "</h5>" + "<p class='note-content'>" + noteContent + "</p>";
     noteList.appendChild(newElement);
 };
 
 var setupButton = document.getElementById("setup-button");
-setupButton.addEventListener("click", testSetup)
+setupButton.addEventListener("click", testSetup);
 var resetButton = document.getElementById("reset-button")
-resetButton.addEventListener("click", testReset)
+resetButton.addEventListener("click", testReset);
+var submitButton = document.getElementById("submit-save");
+submitButton.addEventListener("click", getSubmit);
 
 function testSetup() {
     var testNotes = [];
@@ -41,5 +54,19 @@ function testSetup() {
 function testReset() {
     var testNotes = [];
     localStorage.setItem("notes", JSON.stringify(testNotes));
+    location.reload();
+}
+
+function getSubmit() {
+    var name = document.getElementById("name").value;
+    var content = document.getElementById("content").value;
+    var newNote = {
+        name: name,
+        content: content,
+        id: getId()
+    }
+    var notesUpdated = notes;
+    notesUpdated.push(newNote);
+    localStorage.setItem("notes", JSON.stringify(notesUpdated));
     location.reload();
 }
